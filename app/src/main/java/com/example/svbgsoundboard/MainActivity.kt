d41f57cce4897daaf5cdca4c675e46c79653c408
stevethewright <stevethewright@live.com>
2023-04-19 22:35:19 +0930

    Updated settings page and button to go to settings page.

--- app/src/main/java/com/example/svbgsoundboard/MainActivity.kt
+++ app/src/main/java/com/example/svbgsoundboard/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.svbgsoundboard
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_view)
+        findViewById<Button>(R.id.settings_button)
+            .setOnClickListener {
+                setContentView(R.layout.settings_view)
+            }
     }
 }
